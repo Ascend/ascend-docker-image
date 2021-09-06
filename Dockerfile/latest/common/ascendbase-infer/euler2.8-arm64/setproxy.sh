f1958a9ed8a3605b538dd17acba30a43947c0bc3
WX950019 <mamincang@huawei.com>
2021-09-06 08:03:05 +0000

    删除文件 Dockerfile/latest/common/ascendbase-infer/euler2.8-arm64/setproxy.sh

--- Dockerfile/latest/common/ascendbase-infer/euler2.8-arm64/setproxy.sh
+++
@@ -1,9 +0,0 @@
-# 文件说明：此文件为镜像步骤前置hook，用于配置外网代理或更换官方yum源和pip源提高镜像构建速度的样例文件
-# 使用说明：去掉相应注释，即可使用相应设置功能，也可以对相应设置自行修改
-# 代码示例：
-
-# 设置代理 按实际情况进行修改
-#export http_proxy=http://user:password@proxyserverip:port
-#export https_proxy=http://user:password@proxyserverip:port
-
-
