88455d7248c5d9db6b36715420401483eeb4237f
WX950019 <mamincang@huawei.com>
2021-09-06 07:56:25 +0000

    删除文件 Dockerfile/latest/common/ascend-tensorflow/postinstall.sh

--- Dockerfile/latest/common/ascend-tensorflow/postinstall.sh
+++
@@ -1,6 +0,0 @@
-# 说明：此文件为镜像步骤后置hook，用于撤销任何在配对preinstall.sh中做出的、不希望保留在最终镜像中的修改
-# 在此撤销配对preinstall.sh中放置的version.info和ascend_install.info文件
-# 代码示例：
-#
-# rm -rf /usr/local/Ascend/driver
-# rm -f /etc/ascend_install.info
