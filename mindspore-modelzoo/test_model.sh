dd4a7e59dabfe0a18dd5d8ac2900019e7c685478
liuyong <1390871916@qq.com>
2023-02-15 20:46:32 +0800

    支持集群训练推理及dmi

--- mindspore-modelzoo/test_model.sh
+++ mindspore-modelzoo/test_model.sh
@@ -3,11 +3,11 @@
 ## 启动训练
 
 function start_test_model() {
-    sed -i 's/epoch_size: 90/epoch_size: 5/' ~/Resnet50_Cifar_for_MindSpore/config/resnet50_cifar10_config.yaml
-    cd ~/Resnet50_Cifar_for_MindSpore/scripts
-    bash ~/Resnet50_Cifar_for_MindSpore/scripts/run_standalone_train.sh ~/Resnet50_Cifar_for_MindSpore/data/cifar10/cifar-10-batches-bin ~/Resnet50_Cifar_for_MindSpore/config/resnet50_cifar10_config.yaml
+    sed -i 's/epoch_size: 90/epoch_size: 5/' ~/samples/Resnet50_Cifar_for_MindSpore/config/resnet50_cifar10_config.yaml
+    cd ~/samples/Resnet50_Cifar_for_MindSpore/scripts
+    bash ~/samples/Resnet50_Cifar_for_MindSpore/scripts/run_standalone_train.sh ~/samples/Resnet50_Cifar_for_MindSpore/data/cifar10/cifar-10-batches-bin ~/samples/Resnet50_Cifar_for_MindSpore/config/resnet50_cifar10_config.yaml
     sleep 5s
-    tail -f ~/Resnet50_Cifar_for_MindSpore/scripts/train/log &
+    tail -f ~/samples/Resnet50_Cifar_for_MindSpore/scripts/train/log &
     local wait_time
     wait_time=0
     while true; do
