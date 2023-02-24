5a38fd0d132c8881768de76ba4c74d9564cbca8a
zhaoyue <1309344734@qq.com>
2023-02-24 11:23:37 +0800

    tensorflow1.15-modelzoo增加单机8卡训练demo

--- mindspore-modelzoo/test_model.sh
+++ mindspore-modelzoo/test_model.sh
@@ -3,11 +3,11 @@
 ## 启动训练
 
 function start_test_model() {
-    sed -i 's/epoch_size: 90/epoch_size: 5/' ~/samples/Resnet50_Cifar_for_MindSpore/config/resnet50_cifar10_config.yaml
-    cd ~/samples/Resnet50_Cifar_for_MindSpore/scripts
-    bash ~/samples/Resnet50_Cifar_for_MindSpore/scripts/run_standalone_train.sh ~/samples/Resnet50_Cifar_for_MindSpore/data/cifar10/cifar-10-batches-bin ~/samples/Resnet50_Cifar_for_MindSpore/config/resnet50_cifar10_config.yaml
+    sed -i 's/epoch_size: 90/epoch_size: 5/' /home/HwHiAiUser/samples/Resnet50_Cifar_for_MindSpore/config/resnet50_cifar10_config.yaml
+    cd /home/HwHiAiUser/samples/Resnet50_Cifar_for_MindSpore/scripts
+    bash /home/HwHiAiUser/samples/Resnet50_Cifar_for_MindSpore/scripts/run_standalone_train.sh /home/HwHiAiUser/samples/Resnet50_Cifar_for_MindSpore/data/cifar10/cifar-10-batches-bin /home/HwHiAiUser/samples/Resnet50_Cifar_for_MindSpore/config/resnet50_cifar10_config.yaml
     sleep 5s
-    tail -f ~/samples/Resnet50_Cifar_for_MindSpore/scripts/train/log &
+    tail -f /home/HwHiAiUser/samples/Resnet50_Cifar_for_MindSpore/scripts/train/log &
     local wait_time
     wait_time=0
     while true; do
