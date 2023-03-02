9f2be5bfa646201552553dcb7e08b3ea5ffb00bd
liuyong <1390871916@qq.com>
2023-03-02 20:15:39 +0800

    增加pytorch集群训练样例

--- pytorch-modelzoo/test_model.sh
+++ pytorch-modelzoo/test_model.sh
@@ -3,19 +3,19 @@
 ## 启动训练
 
 function start_test_model() {
-    cd ~/Resnet50_Cifar_for_PyTorch
+    cd ~/samples/Resnet50_Cifar_for_PyTorch
     bash test/train_performance_$1.sh &
     sleep 5s
     tail -f test/output/0/train_0.log &
     while true;do
         sleep 20
         wait_time=$((wait_time+20))
-        if [ "$(grep -c "THPModule_npu_shutdown success" ~/Resnet50_Cifar_for_PyTorch/test/output/0/train_0.log)" -gt 0 ] && [ "$(grep -c "Saving checkpoint at 2 epochs" ~/Resnet50_Cifar_for_PyTorch/test/output/0/train_0.log)" -gt 0 ];then
+        if [ "$(grep -c "THPModule_npu_shutdown success" ~/samples/Resnet50_Cifar_for_PyTorch/test/output/0/train_0.log)" -gt 0 ] && [ "$(grep -c "Saving checkpoint at 2 epochs" ~/Resnet50_Cifar_for_PyTorch/test/output/0/train_0.log)" -gt 0 ];then
             sleep 20
             echo test pytorch-modelzoo model success
             exit 0
         fi
-        if [ "$(grep -c "Error" ~/Resnet50_Cifar_for_PyTorch/test/output/0/train_0.log)" -gt 0 ] || [ ${wait_time} -eq 1000 ];then
+        if [ "$(grep -c "Error" ~/samples/Resnet50_Cifar_for_PyTorch/test/output/0/train_0.log)" -gt 0 ] || [ ${wait_time} -eq 1000 ];then
             echo test pytorch-modelzoo model failed
             exit 1
         fi 
