4a915502dabe0ec03293ff82b93ac06c1619dc01
liuyong <1390871916@qq.com>
2023-03-15 10:23:19 +0800

    增加tf2集群训练样例

--- tensorflow2.6.5-modelzoo/test_model.sh
+++ tensorflow2.6.5-modelzoo/test_model.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 function start_test_model() {
-    cd ~/Keras-MnasNet_ID3518_for_TensorFlow2.X/test
+    cd ~/samples/Keras-MnasNet_ID3518_for_TensorFlow2.X/test
     . /usr/local/Ascend/tfplugin/set_env.sh && . /usr/local/Ascend/ascend-toolkit/set_env.sh
-    bash train_full_1p_static.sh --data_path=/home/HwHiAiUser/Keras-MnasNet_ID3518_for_TensorFlow2.X/data/cifar10/cifar-10-batches-py/ --train_epochs=20&
+    bash train_full_1p_static.sh --data_path=/home/HwHiAiUser/samples/Keras-MnasNet_ID3518_for_TensorFlow2.X/data/cifar10/cifar-10-batches-py/ --train_epochs=20&
     sleep 5
     tail -f output/train_.log &
     while true;do
         sleep 20
         wait_time=$((wait_time+20))
-        if [ "$(grep -c "Stop graph engine succeed" ~/Keras-MnasNet_ID3518_for_TensorFlow2.X/test/output/train_.log)" -gt 0 ] && [ "$(grep -c Epoch ~/Keras-MnasNet_ID3518_for_TensorFlow2.X/test/output/train_.log)" -gt 0 ];then
+        if [ "$(grep -c "Stop graph engine succeed" ~/samples/Keras-MnasNet_ID3518_for_TensorFlow2.X/test/output/train_.log)" -gt 0 ] && [ "$(grep -c Epoch ~/samples/Keras-MnasNet_ID3518_for_TensorFlow2.X/test/output/train_.log)" -gt 0 ];then
             sleep 20
             echo test tensorflow-modelzoo model success
             exit 0
         fi
-        if [ "$(grep -c "Error" ~/Keras-MnasNet_ID3518_for_TensorFlow2.X/test/output/train_.log)" -gt 0 ] || [ ${wait_time} -eq 1000 ];then
+        if [ "$(grep -c "Error" ~/samples/Keras-MnasNet_ID3518_for_TensorFlow2.X/test/output/train_.log)" -gt 0 ] || [ ${wait_time} -eq 1000 ];then
             echo test tensorflow-modelzoo model failed
             exit 1
         fi 
