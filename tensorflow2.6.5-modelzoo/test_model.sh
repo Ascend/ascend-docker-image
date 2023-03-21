e2f861703e7ded6ab79f121f22d3ee4ab09c9340
zhaoyue <1309344734@qq.com>
2023-03-21 20:36:00 +0800

    debug

--- tensorflow2.6.5-modelzoo/test_model.sh
+++ tensorflow2.6.5-modelzoo/test_model.sh
@@ -22,7 +22,7 @@ function start_test_model() {
         done
     fi
     if [ $1 == 8p ];then
-        export framework=TensorFlow
+        export framework=Tensorflow
         cd /home/HwHiAiUser/samples/ResNet50_ID0360_for_TensorFlow2.X/test
         bash train_start.sh /home/HwHiAiUser/samples/ResNet50_ID0360_for_TensorFlow2.X/ /home/HwHiAiUser/samples/logs tensorflow/resnet_ctl_imagenet_main.py --data_dir=/job/data --distribution_strategy=one_device --use_tf_while_loop=true --epochs_between_evals=1 --skip_eval --enable_checkpoint_and_export --model_dir=/home/HwHiAiUser/samples/ --batch_size=2048 --train_epochs=5 --base_learning_rate=3.96 --over_dump=False --data_dump_flag=False --data_dump_step=10 --profiling=False --autotune=False --warmup_epochs=5 --label_smoothing=0.1 --weight_decay=0.000025 --num_accumulation_steps=1 --train_steps=625 --optimizer=SGD --lr_schedule=polynomial --drop_eval_remainder=True --eval_offset_epochs=2
     fi
